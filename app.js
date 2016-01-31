3ccd825166ceae9c549aa69a9ed21fefaa230e44
Astha Sharma <a@astha.me>
2016-01-31 10:54:06 +0530

    Set the clear and clearone logic

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var current = "";
-console.log("hi");
+
 function addDigit(val){
 	current += val;
 	console.log(current);
 	document.getElementById("result").value = current;
+}
+
+function clearall(){
+	console.log(current);
+	current="";
+	document.getElementById("result").value = "0";
+}
+
+function clear(){
+	current += "hi";
+	document.getElementById("result").value = current;
+}
+
+function clearone(){
+	console.log(current);
+	current = current.substr(0, current.length-1);
+	if(current.length > 0){
+		document.getElementById("result").value = current;
+	} else{
+		document.getElementById("result").value = "0";
+	}
 }
